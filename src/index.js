2d61e1282090a30a8f5dfb319d942446874a10dd
Anmol <anmoldhage999@gmail.com>
2022-01-11 20:31:11 +0530

    update pre-loader

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import App from './Components/App.jsx';
 
 let loader = document.getElementById("loader");
 
-const myTimeout = setTimeout(function () {
-  loader.style.display = "none";
-}, 2000);
+window.addEventListener('load', function () {
+  const myTimeout = setTimeout(() => {
+    loader.style.display = "none";
+  }, 2000);
+})
 
 ReactDOM.render(
   <App />
